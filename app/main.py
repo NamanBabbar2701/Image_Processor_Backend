f8ce4e8ddd625e5cdf856697a261d4459da32af6
NamanBabbar2701 <MITU22BTCS0467@students.mituniversity.edu.in>
2026-07-28 18:56:13 +0530

    CORS Update

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://image-processor.vercel.app/",
+        "https://image-processor-alpha.vercel.app",
         ],  
     allow_credentials=True,
     allow_methods=["*"],
